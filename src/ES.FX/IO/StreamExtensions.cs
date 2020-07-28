fa5da3dbd03bee5b3f618da37b610ab208d889ec
Romeo Dumitrescu <winromulus@winromulus.com>
2020-07-28 23:03:22 +0300

    Updated pipelines and restructured repository
    Co-authored-by: Romeo Dumitrescu <winromulus@users.noreply.github.com>

---
+++ src/ES.FX/IO/StreamExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ES.FX.IO
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ToByteArray(this Stream stream)
+        {
+            if (stream is null) throw new ArgumentNullException(nameof(stream));
+            if (stream is MemoryStream directMemoryStream) return directMemoryStream.ToArray();
+
+            using var memoryStream = new MemoryStream();
+            stream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+
+        public static async Task<byte[]> ToByteArrayAsync(this Stream stream, CancellationToken cancellationToken = default)
+        {
+            if (stream is null) throw new ArgumentNullException(nameof(stream));
+            if (stream is MemoryStream directMemoryStream) return directMemoryStream.ToArray();
+
+            using var memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream, 81920, cancellationToken);
+            return memoryStream.ToArray();
+        }
+    }
+}
